c65214a7bc27808aee6f3c0ffe2bf3652651b124
habul17 <abulhassanca.dev@gmail.com>
2026-07-03 16:42:53 +0530

    Complete Backend

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const { authMiddleware } = require('./middleware');
+const path = require('path');
 require('dotenv').config();
 
 express.use(express.json());
@@ -57,6 +59,59 @@ app.post("/signin", (req, res) => {
         token
     })
 
+})
+
+
+// POST - Create Note
+
+app.post("/notes", authMiddleware, (req, res) => {
+
+    const note = req.body.note;
+    userName = req.userName;
+
+    NOTES.push({
+        note,
+        userName
+    })
+
+    res.json({
+        message: "Note Created Successfully"
+    })
+})
+
+
+// GET - Get All Notes
+
+app.get("/notes", (req, res) => {
+
+    const userName = req.userName;
+
+    const userNotes = NOTES.filter(notes => notes.userName === userName);
+
+    res.json({
+        notes: userNotes
+    })
+
+})
+
+// GET - Serve Signup Page
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'signup.html'));
+})
+
+
+// GET - Serve Signin Page
+
+app.get("/signin", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'signin.html'));
+})
+
+
+// GET - Serve FE
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 })
 
 app.listen(3000);
